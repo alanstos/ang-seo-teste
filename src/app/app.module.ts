88585ca2b9f2591c498d0b7ee66f735fd43d6c90
Alan Araujo dos Santos <alan@MacBook-Pro-de-Alan.local>
2018-02-13 22:33:01 -0200

    inicio basico

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { PostsComponent } from './blog/posts/posts.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostsComponent
   ],
   imports: [
     BrowserModule
